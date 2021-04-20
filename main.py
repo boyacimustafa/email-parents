1225cd2a541c4dd125b5198d924d6f659aff5ad9
Mustafa Boyar <76089544+boyacimustafa@users.noreply.github.com>
2021-04-19 23:50:45 -0700

    import grades
    I was trying to get grades directly from our school information system but the API was not accessible to me. So I exported all the grades of all students to an excel file and go from there.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello')
+import pandas
+import os
+os.chdir('C:\\Users\\mustafa boyar\\documents\github\\email-parents')
+
+def load_filter_sort(xl_file):
+    allgrades_xl = pandas.read_excel(xl_file)
+    failing_semester2grades=allgrades_xl[(allgrades_xl["grading_termName"]=="Semester 2") & (allgrades_xl["grading_task"]=="End of Course Grade") & (allgrades_xl["grading_progressPercent"]<60)]
+    failing_semester2grades.sort_values(by=['student_stateID'])
+    return failing_semester2grades
+
+
+failing_students=load_filter_sort("allgrades.xlsx")
+print(failing_students)
